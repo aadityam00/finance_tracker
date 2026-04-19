9211c8c495bc19fa16c9dda32ccd95d7c185fa1c
aadityam00 <aadityavijaym2005@gmail.com>
2026-04-19 21:51:06 +0530

    Add Vercel configs and set API base URL to Render

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import axios from 'axios'
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || ''
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'https://finance-tracker-w00v.onrender.com'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
