a33dcd64355aa286a10b115c6b7ee5ae72dbb100
aadityam00 <aadityavijaym2005@gmail.com>
2026-04-19 21:03:01 +0530

    Separate backend into its own repository

--- backend/utils/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-// Wrapper for async route controllers to handle try/catch automatically
-const asyncHandler = (fn) => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-module.exports = asyncHandler;
